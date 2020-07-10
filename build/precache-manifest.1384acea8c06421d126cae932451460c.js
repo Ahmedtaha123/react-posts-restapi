3dee3cfd0f8c29e4f22f388948525a7df0add7d8
Ahmedtaha123 <ahmed@maakaweb.com>
2020-07-10 18:39:30 +0200

    add build folder

---
+++ build/precache-manifest.1384acea8c06421d126cae932451460c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d26825264c73f4d7f4f8",
+    "url": "/static/css/main.1a93adcb.chunk.css"
+  },
+  {
+    "revision": "d26825264c73f4d7f4f8",
+    "url": "/static/js/main.d2682526.chunk.js"
+  },
+  {
+    "revision": "e6db63336e07aca17f22",
+    "url": "/static/js/1.e6db6333.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97a227e9caa54847010a23355c7183b6",
+    "url": "/index.html"
+  }
+];
